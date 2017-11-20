199480f01a0f8c38eb4498cc09a13fbf237eabaa
Sylvia <sylviawei19950920@gmail.com>
2017-11-20 10:59:42 +0800

    navbar

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -21,11 +21,11 @@ from django.contrib import admin
 from .views import home_page,about_page,contact_page,login_page,register_page
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^contact/$', contact_page),
-    url(r'^about/$', about_page),
-    url(r'^login/$', login_page),
-    url(r'^register/$', register_page),
-    url(r'^$', home_page),
+    url(r'^contact/$', contact_page,name='contact'),
+    url(r'^about/$', about_page,name='about'),
+    url(r'^login/$', login_page,name='login'),
+    url(r'^register/$', register_page,name='register'),
+    url(r'^$', home_page,name='home'),
     url(r'^products/',include('products.urls',namespace='products')),
 ]
 if settings.DEBUG:
--- src/products/urls.py
+++ src/products/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import ProductDetaiSlugView,ProductListView
 
 urlpatterns = [
-    url(r'^$', ProductListView.as_view(),name='product_list'),
-    url(r'(?P<slug>[\w_]+)/$', ProductDetaiSlugView.as_view(),name='product_detail'),
+    url(r'^$', ProductListView.as_view(),name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', ProductDetaiSlugView.as_view(),name='detail'),
 
 ]
