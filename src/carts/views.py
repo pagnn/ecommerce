1427e98fc4c397c18bdc082c4cbe48019a4c1fd0
Sylvia <sylviawei19950920@gmail.com>
2017-11-23 11:48:44 +0800

    Finish the Cart app.

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from products.models import Product
 from .models import Cart
 # Create your views here.
 def cart_home(request):
 	cart_obj,new_obj=Cart.objects.new_or_get(request)
-	products=cart_obj.products.all()
-	total = 0
-	for x in products:
-		total+=x.price
-	print (total)
-	cart_obj.total=total
-	cart_obj.save()
-	return render(request,'carts/home.html',{})
+	return render(request,'carts/home.html',{'cart':cart_obj})
+
+def cart_update(request):
+	product_id=request.POST.get('product_id')
+	if product_id is not None:
+		try:
+			obj=Product.objects.get(id=product_id)
+		except Product.DoesNotExist:
+			print('Show message to the user')
+			redirect("carts:home")
+		cart_obj,new_obj=Cart.objects.new_or_get(request)
+		if obj in cart_obj.products.all():
+			cart_obj.products.remove(obj)
+		else:
+			cart_obj.products.add(obj)
+	request.session['cart_items']=cart_obj.products.count()
+	return redirect("carts:home")
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from django.views.generic import ListView,DetailView
 from django.http import Http404
+
+from carts.models import Cart
 from .models import Product
 
 # Create your views here.
@@ -9,6 +11,11 @@ class ProductListView(ListView):
 		request=self.request
 		return Product.objects.all()
 class ProductDetaiSlugView(DetailView):
+	def get_context_data(self,*args,**kwargs):
+		context=super(ProductDetaiSlugView,self).get_context_data(*args,**kwargs)
+		cart_obj,new_obj=Cart.objects.new_or_get(self.request)
+		context['cart']=cart_obj
+		return context
 	def get_queryset(self,*args,**kwargs):
 		request=self.request
 		return Product.objects.all()
