effb501f1723be431816396d05207ad058069793
Sylvia <sylviawei19950920@gmail.com>
2017-12-01 09:21:21 +0800

    Upgrade the objectviewed

--- analytics/models.py
+++ analytics/models.py
@@ -34,11 +34,14 @@ class ObjectViewed(models.Model):
 
 def object_viewed_receiver(sender,instance,request,*args,**kwargs):
 	c_type=ContentType.objects.get_for_model(sender)
-	new_view_obj=ObjectViewed.objects.create(
-			user=request.user,
-			ip_address=get_client_ip(request),
-			content_type=c_type,
-			object_id=instance.id,
+	user=None
+	if request.user.is_authenticated():
+		user=request.user
+		new_view_obj=ObjectViewed.objects.create(
+				user=user,
+				ip_address=get_client_ip(request),
+				content_type=c_type,
+				object_id=instance.id,
 		)
 
 object_viewed_signal.connect(object_viewed_receiver)
